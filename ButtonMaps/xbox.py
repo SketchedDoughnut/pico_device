24fd93e84e6635be9e7b07061e515b3d2ba02448
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2025-08-16 14:41:35 -0700

    Create xbox.py

---
+++ ButtonMaps/xbox.py
@@ -0,0 +1,17 @@
+class BUTTONS:
+    A = 0
+    B = 1
+    X = 2
+    Y = 3
+    LeftBumper = 4
+    RightBumper = 5
+    LeftTrigger = 6
+    RightTrigger = 7
+    VIEW_BACK = 8
+    MENU_START = 9
+    LEFT_JOYSTICK = 10
+    RIGHT_JOYSTICK = 11
+    PAD_UP = 12
+    PAD_DOWN = 13
+    PAD_LEFT = 14
+    PAD_RIGHT = 15
