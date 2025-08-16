f5edefc50a04cfb65921bf2b8fee048d2c83edf4
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2025-08-16 15:08:16 -0700

    Update Controller.py

--- Devices/Controller.py
+++ Devices/Controller.py
@@ -11,6 +11,10 @@ class ControllerInterface:
     def __init__(self):
         self.gamepad = None
         self.buttons = 0
+        self.LEFT_X = 'lx'
+        self.LEFT_Y = 'ly'
+        self.RIGHT_X = 'rx'
+        self.RIGHT_Y = 'ry'
         self.x = 0 # left x?
         self.y = 0 # left y?
         self.z = 0 # right x?
@@ -25,13 +29,31 @@ class ControllerInterface:
         return
     
     def setButton(self, button: int, value: int) -> None:
+        '''
+        Sets the button to either on (1) or off (0).
+        '''
         if value: self.buttons |= (1 << button)
         else: self.buttons &= ~(1 << button)
         return
-        
-    def resetButtons(self): self.buttons = 0
+    
+    # TODO
+    def setJoystick(self, joystick: str, value: int) -> None:
+        pass
+    
+    # TODO
+    def moveJoystickBy(self, joystick: str, value: int) -> None:
+        pass
+    
+    def resetButtons(self):
+        '''
+        Resets all of the buttons to off (0).
+        '''
+        self.buttons = 0
     
     def sendReport(self) -> None:
+        '''
+        Sends the report of the inputs to the connected device.
+        '''
         report = bytearray(6)
         report[0] = self.buttons & 0xFF        # low byte  (buttons 1–8)
         report[1] = (self.buttons >> 8) & 0xFF # high byte (buttons 9–16)
