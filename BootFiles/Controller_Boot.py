49ef5b42d3231ac4cd436d95fd8b8b569d5bcde2
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2025-08-16 20:43:31 -0700

    Update Controller_Boot.py

--- BootFiles/Controller_Boot.py
+++ BootFiles/Controller_Boot.py
@@ -1,18 +1,18 @@
-'''
-REMEMBER TO PUT THIS INTO THE BOOT.PY FILE!
-'''
-
 # boot.py
 import usb_hid
 
 # This is only one example of a gamepad descriptor.
 # It may not suit your needs, or be supported on your host computer.
 
+# HELPFUL INFORMATION: https://eleccelerator.com/tutorial-about-usb-hid-report-descriptors/
+
 GAMEPAD_REPORT_DESCRIPTOR = bytes((
     0x05, 0x01,  # Usage Page (Generic Desktop Ctrls)
     0x09, 0x05,  # Usage (Game Pad)
     0xA1, 0x01,  # Collection (Application)
-    0x85, 0x04,  #   Report ID (4)
+    0x85, 0x04,  # Report ID (4)
+    
+    # buttons - 2 bytes
     0x05, 0x09,  #   Usage Page (Button)
     0x19, 0x01,  #   Usage Minimum (Button 1)
     0x29, 0x10,  #   Usage Maximum (Button 16)
@@ -21,16 +21,19 @@ GAMEPAD_REPORT_DESCRIPTOR = bytes((
     0x75, 0x01,  #   Report Size (1)
     0x95, 0x10,  #   Report Count (16)
     0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    
+    # joysticks - 4 bytes
     0x05, 0x01,  #   Usage Page (Generic Desktop Ctrls)
     0x15, 0x81,  #   Logical Minimum (-127)
     0x25, 0x7F,  #   Logical Maximum (127)
     0x09, 0x30,  #   Usage (X)
     0x09, 0x31,  #   Usage (Y)
     0x09, 0x32,  #   Usage (Z)
-    0x09, 0x35,  #   Usage (Rz)
+    0x09, 0x33,  #   Usage (Rx)
     0x75, 0x08,  #   Report Size (8)
     0x95, 0x04,  #   Report Count (4)
     0x81, 0x02,  #   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    
     0xC0,        # End Collection
 ))
 
